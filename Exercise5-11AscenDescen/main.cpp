ed6f35df70adcb5d425a64d2a92a7b4fe59a521c
Du Park <dupark3@gmail.com>
2018-02-21 16:25:35 -0500

    Check for longest word within first pass

--- Exercise5-11AscenDescen/main.cpp
+++ Exercise5-11AscenDescen/main.cpp
@@ -12,19 +12,20 @@ typedef std::string::size_type string_size;
 int main() {
     // Obtain input using read function for unknown number of words.
     // Using vector since the only need for this container is to add elements in the back
+    // read function also stores the length of the longest word in maxLen, so that we can size the output table later
     std::cout << "Input string to check for absence of ascenders and descenders: "
               << std::endl;
     std::vector<std::string> words;
     string_size maxLen = 0;
     read(std::cin, words, maxLen);
 
+    std::string maxString; // will store the longest word without ascenders or descenders
+
+    // set up the output table header
     std::cout << std::endl
               << "Word" << std::string(maxLen, ' ')
               << " Ascenders?  Descenders?" << std::endl
-              << std::string(maxLen + 27, '-') << std::endl;
-
-    // Create new vector to store words without ascenders or descenders
-    std::vector<std::string> noAscNoDesc;
+              << std::string(maxLen + 27, '-') << std::endl; // underline table heading
 
     // Use index i to march through the vector, passing each word into noAscenders function
     for (vec_size i = 0; i != words.size(); ++i){
@@ -32,28 +33,31 @@ int main() {
         // Write word in first column
         std::cout << words[i] << std::string(maxLen - words[i].size(), ' ');
 
+        // Write Yes or No on second and third column for ascenders and descenders, respectively
         if (noAscenders(words[i])){
             std::cout << "       No          ";
             noAsc = true;
-        } else
+        } else {
             std::cout << "       Yes         ";
-
+        }
         if (noDescenders(words[i])){
             std::cout << " No";
             if (noAsc == true)
-                noAscNoDesc.push_back(words[i]);
-        } else
+                maxString = words[i].size() > maxString.size()? words[i] : maxString;
+        } else{
             std::cout << " Yes";
+        }
 
-        std::cout << std::endl << std::endl;
+        std::cout << std::endl;
     }
 
-    // Iterate through vector to store the longest word as a string and print tat string
-    std::string maxString;
-    for (vec_size j = 0; j != noAscNoDesc.size(); ++j){
-        maxString = noAscNoDesc[j].size() > maxString.size()? noAscNoDesc[j] : maxString;
-    }
-    std::cout << "The longest word without Ascenders or Descenders is: " << maxString << std::endl;
+
+
+    // Print the longest word without ascenders or descenders
+    std::cout << std::endl
+              << "The longest word without Ascenders or Descenders is: "
+              << maxString
+              << std::endl;
 
     return 0;
 }
